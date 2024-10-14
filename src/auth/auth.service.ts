ee6d84b6f17719926348b9a4a7707132270b7bb6
Udoh Lawrence <lawrenceaudoh@gmail.com>
2024-10-14 08:10:17 +0100

    UPDATE: scaffold the auth controller/provider

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+export class AuthService{
+  login(){
+    return {message: 'I am logged in'};
+  }
+
+  signup(){
+    return {message: 'I have signed up'};
+  }
+}
