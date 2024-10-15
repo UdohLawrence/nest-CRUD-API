92daa412c851ee98a95b89e02e68bb816540eba3
Udoh Lawrence <lawrenceaudoh@gmail.com>
2024-10-15 08:59:03 +0100

    FEATURE: add validation to signup fields

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2'
 
-@Injectable({})
+@Injectable()
 export class AuthService{
+
+  constructor(private prisma: PrismaService){}
+
   login(){
     return {message: 'I am logged in'};
   }
 
-  signup(){
-    return {message: 'I have signed up'};
+  async signup(signUpDto: AuthDto){
+
+    const hash = await argon.hash(signUpDto.password);  //generate the password hash
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: signUpDto.email,
+        hash,
+      }
+    })
+    return user;
   }
 }
